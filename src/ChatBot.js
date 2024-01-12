0925626d7d6133f0807952decbd8c8daded4df70
Dallon <dboultbee@hotmail.com>
2024-01-12 15:11:09 -0800

    basica chatbot.js template not used currently

---
+++ src/ChatBot.js
@@ -0,0 +1,15 @@
+import "react"
+import "react-dom"
+  
+export default function ChatBot(){
+  
+  
+
+  return (
+    <div>
+      <input></input>
+      <button> Send </button>
+    </div>
+  )
+  
+}
