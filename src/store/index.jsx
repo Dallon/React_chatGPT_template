98264c02ff20c0c96a9ab1a200e305366e34bc43
Dallon <dboultbee@hotmail.com>
2024-02-18 09:11:23 -0800

    not using other reducers yet, added chatSlice

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,12 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import conversationsSlice from "./slices/conversationsSlice";
 import chatbotStatusSlice from "./slices/chatbotStatusSlice";
+import { chatSlice } from "./slices/chatSlice";
 
 const store = configureStore({
-  reducers: {
-    conversations: conversationsSlice.reducer,
-    chatbotStatus: chatbotStatusSlice.reducer
+  reducer: {
+    chat: chatSlice.reducer, // 
   },
 });
-
 export default store;
